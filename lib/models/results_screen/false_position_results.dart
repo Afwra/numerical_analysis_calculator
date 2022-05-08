2c96bb88f31047d5812e109fc3571889f1e8604f
Afwra <ahmedgamal5179@gmail.com>
2022-05-08 19:38:28 +0200

    update v2: - added error checking - added conditional builder - improved performance

--- lib/models/results_screen/false_position_results.dart
+++ lib/models/results_screen/false_position_results.dart
@@ -2,8 +2,10 @@ import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter/material.dart';
 import 'package:math_keyboard/math_keyboard.dart';
 import 'package:math_expressions/math_expressions.dart';
+import 'package:numerical_analysis_calculator/models/error_screen.dart';
 import 'package:numerical_analysis_calculator/modules/result_model.dart';
 import '../../shared/components/components.dart';
+import 'package:buildcondition/buildcondition.dart';
 
 class FalsePositionResultsScreen extends StatefulWidget {
 
@@ -26,23 +28,27 @@ class _FalsePositionResultsScreenState extends State<FalsePositionResultsScreen>
   void initState() {
     // TODO: implement initState
     super.initState();
-    calculateFalsePosition();
   }
 
   @override
   Widget build(BuildContext context) {
+    double root =  calculateFalsePosition();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Results'),
         backgroundColor: HexColor("21325E"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: ListView.separated(
-          itemBuilder: (context,index){return SingleChildScrollView(child: itemBuilder(results[index]),);},
-          separatorBuilder: (context,index)=>const SizedBox(height: 15,),
-          itemCount: results.length,
+      body: BuildCondition(
+        condition: root != 0,
+        builder:(context) => Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: ListView.separated(
+            itemBuilder: (context,index){return SingleChildScrollView(child: itemBuilder(results[index]),);},
+            separatorBuilder: (context,index)=>const SizedBox(height: 15,),
+            itemCount: results.length,
+          ),
         ),
+        fallback: (context) => const ErrorScreen(),
       ),
       backgroundColor: HexColor("3E497A"),
     );
@@ -67,52 +73,66 @@ class _FalsePositionResultsScreenState extends State<FalsePositionResultsScreen>
     // double eval  = mathExpression.evaluate(EvaluationType.REAL, cm);
     // print(eval);
 
-    int iter = 0;
-    double xr = 0;
-    double xrold = 0;
-    double error = 0;
-    do
-    {
-      cm.bindVariableName('x', Number(widget.xl));
-      mathExpression.evaluate(EvaluationType.REAL,cm);
-      double funXl = mathExpression.evaluate(EvaluationType.REAL, cm);
+    cm.bindVariableName('x', Number(widget.xl));
+    mathExpression.evaluate(EvaluationType.REAL,cm);
+    double funXl = mathExpression.evaluate(EvaluationType.REAL, cm);
 
-      cm.bindVariableName('x', Number(widget.xu));
-      mathExpression.evaluate(EvaluationType.REAL,cm);
-      double funXu = mathExpression.evaluate(EvaluationType.REAL, cm);
+    cm.bindVariableName('x', Number(widget.xu));
+    mathExpression.evaluate(EvaluationType.REAL,cm);
+    double funXu = mathExpression.evaluate(EvaluationType.REAL, cm);
 
-      xrold = xr;
-      xr =widget.xu - (funXu*(widget.xl - widget.xu)) / (funXl - funXu);
-      error = (((xr - xrold) / xr) * 100).abs();
+    if(funXu * funXl > 0){
+      return 0;
+    }
+    else{
+      int iter = 0;
+      double xr = 0;
+      double xrold = 0;
+      double error = 0;
+      do
+      {
+        cm.bindVariableName('x', Number(widget.xl));
+        mathExpression.evaluate(EvaluationType.REAL,cm);
+        double funXl = mathExpression.evaluate(EvaluationType.REAL, cm);
 
-      cm.bindVariableName('x', Number(widget.xl));
-      mathExpression.evaluate(EvaluationType.REAL,cm);
-      funXl = mathExpression.evaluate(EvaluationType.REAL, cm);
+        cm.bindVariableName('x', Number(widget.xu));
+        mathExpression.evaluate(EvaluationType.REAL,cm);
+        double funXu = mathExpression.evaluate(EvaluationType.REAL, cm);
 
-      cm.bindVariableName('x', Number(widget.xu));
-      mathExpression.evaluate(EvaluationType.REAL,cm);
-      funXu = mathExpression.evaluate(EvaluationType.REAL, cm);
+        xrold = xr;
+        xr =widget.xu - (funXu*(widget.xl - widget.xu)) / (funXl - funXu);
+        error = (((xr - xrold) / xr) * 100).abs();
 
-      cm.bindVariableName('x', Number(xr));
-      mathExpression.evaluate(EvaluationType.REAL,cm);
-      double funXr = mathExpression.evaluate(EvaluationType.REAL, cm);
+        cm.bindVariableName('x', Number(widget.xl));
+        mathExpression.evaluate(EvaluationType.REAL,cm);
+        funXl = mathExpression.evaluate(EvaluationType.REAL, cm);
 
-      results.add(ResultModel(iter: iter,xl: widget.xl, funXl: funXl, xu: widget.xu, funXu: funXu, xr: xr, funXr: funXr, error: error));
+        cm.bindVariableName('x', Number(widget.xu));
+        mathExpression.evaluate(EvaluationType.REAL,cm);
+        funXu = mathExpression.evaluate(EvaluationType.REAL, cm);
+
+        cm.bindVariableName('x', Number(xr));
+        mathExpression.evaluate(EvaluationType.REAL,cm);
+        double funXr = mathExpression.evaluate(EvaluationType.REAL, cm);
+
+        results.add(ResultModel(iter: iter,xl: widget.xl, funXl: funXl, xu: widget.xu, funXu: funXu, xr: xr, funXr: funXr, error: error));
 
 
-      if(funXl * funXr <0){
-        widget.xu =xr;
-      }
-      else if(funXl * funXr > 0){
-        widget.xl = xr;
-      }
-      else{
-        break;
-      }
-      iter++;
-    } while (error > widget.eps);
+        if(funXl * funXr <0){
+          widget.xu =xr;
+        }
+        else if(funXl * funXr > 0){
+          widget.xl = xr;
+        }
+        else{
+          break;
+        }
+        iter++;
+      } while (error > widget.eps);
 
-    return xr;
+      return xr;
+    }
+
 
   }
 
