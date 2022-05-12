e1ffa72be5b18202fd9600989fe811c6767aa792
Afwra <ahmedgamal5179@gmail.com>
2022-05-12 17:22:04 +0200

    update v10: - added ch3 methods
    need to do:
    - add ch2 methods
    - ui enhancements

--- lib/modules/ch3_screens/golden_section_results_screen.dart
+++ lib/modules/ch3_screens/golden_section_results_screen.dart
@@ -1,84 +1,168 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:numerical_analysis_calculator/models/golden_section_model.dart';
-// import 'package:math_expressions/math_expressions.dart';
+import 'package:math_expressions/math_expressions.dart';
 import 'package:math_keyboard/math_keyboard.dart';
-// import 'package:lazy_data_table/lazy_data_table.dart';
+import 'package:lazy_data_table/lazy_data_table.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class GoldenSectionResultsScreen extends StatelessWidget {
 
-  List<GoldenSectionModel> results =[];
-  List<String> tableHeaders = ['Xl','F(Xl)','X2','F(X2)','X1','F(X1)','XU','F(XU)','D'];
+  List<GoldenSectionModel> results = [];
+  List<String> tableHeaders = [
+    'Xl',
+    'F(Xl)',
+    'X2',
+    'F(X2)',
+    'X1',
+    'F(X1)',
+    'XU',
+    'F(XU)',
+    'D'
+  ];
   double xl;
   double xu;
   int iter;
+  bool min;
   MathFieldEditingController equationController;
+
+  //goldenSection
+  double r = ((sqrt(5) - 1) / 2);
+  double d = 0;
+  double x1 = 0;
+  double x2 = 0;
 
 
   GoldenSectionResultsScreen({Key? key,
-     required this.xl,required this.xu,required this.iter,required this.equationController
+    required this.xl, required this.xu, required this.iter, required this.equationController, required this.min
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    calculateGoldenSection();
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Results'),
-          backgroundColor: HexColor("3F72AF"),
+      appBar: AppBar(
+        title: const Text('Results'),
+        backgroundColor: HexColor("3F72AF"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: LazyDataTable(
+                rows: results.length,
+                columns: 9,
+                tableDimensions: const LazyDataTableDimensions(
+                  cellHeight: 50,
+                  cellWidth: 100,
+                  topHeaderHeight: 50,
+                  leftHeaderWidth: 75,
+                ),
+                topHeaderBuilder: (i) => Center(child: Text(tableHeaders[i])),
+                leftHeaderBuilder: (i) =>
+                    Center(child: Text((results[i].iter).toString())),
+                dataCellBuilder: (i, j) {
+                  if (j == 0) {
+                    return Center(
+                        child: Text(results[i].xl.toStringAsFixed(3)));
+                  }
+                  else if (j == 1) {
+                    return Center(
+                        child: Text(results[i].funXl.toStringAsFixed(3)));
+                  }
+                  else if (j == 2) {
+                    return Center(
+                        child: Text(results[i].x2.toStringAsFixed(3)));
+                  }
+                  else if (j == 3) {
+                    return Center(child: Text(results[i].funX2.toStringAsFixed(
+                        3)));
+                  }
+                  else if (j == 4) {
+                    return Center(child: Text(results[i].x1.toStringAsFixed(
+                        3)));
+                  }
+                  else if (j == 5) {
+                    return Center(child: Text(results[i].funX1.toStringAsFixed(
+                        3)));
+                  }
+                  else if (j == 6) {
+                    return Center(child: Text(results[i].xu.toStringAsFixed(
+                        3)));
+                  }
+                  else if (j == 7) {
+                    return Center(child: Text(
+                        results[i].funXu.toStringAsFixed(3)));
+                  }
+                  else {
+                    return Center(child: Text(results[i].d.toStringAsFixed(3)));
+                  }
+                },
+                topLeftCornerWidget: const Center(child: Text("Iter")),
+              ),
+            ),
+          ],
         ),
-      // body: Padding(
-      //   padding: const EdgeInsets.all(8.0),
-      //   child: Column(
-      //     crossAxisAlignment: CrossAxisAlignment.center,
-      //     children: [
-      //       Expanded(
-      //         child: LazyDataTable(
-      //           rows: results.length,
-      //           columns: 7,
-      //           tableDimensions: const LazyDataTableDimensions(
-      //             cellHeight: 50,
-      //             cellWidth: 100,
-      //             topHeaderHeight: 50,
-      //             leftHeaderWidth: 75,
-      //           ),
-      //           topHeaderBuilder: (i) => Center(child: Text(tableHeaders[i])),
-      //           leftHeaderBuilder: (i) => Center(child: Text(results[i].iter.toString())),
-      //           dataCellBuilder: (i, j) {
-      //             if(j == 0) {
-      //               return Center(child: Text(results[i].xl.toStringAsFixed(3)));
-      //             }
-      //             else if(j==1){
-      //               return Center(child: Text(results[i].funXl.toStringAsFixed(3)));
-      //             }
-      //             else if(j==2){
-      //               return Center(child: Text(results[i].xu.toStringAsFixed(3)));
-      //             }
-      //             else if(j==3){
-      //               return Center(child: Text(results[i].funXu.toStringAsFixed(3)));
-      //             }
-      //             else if(j==4){
-      //               return Center(child: Text(results[i].xr.toStringAsFixed(3)));
-      //             }
-      //             else if(j==5){
-      //               return Center(child: Text(results[i].funXr.toStringAsFixed(3)));
-      //             }
-      //             else{
-      //               return Center(child: Text('${results[i].error.toStringAsFixed(3)}%'));
-      //             }
-      //           },
-      //           topLeftCornerWidget: const Center(child: Text("Iter")),
-      //         ),
-      //       ),
-      //       const SizedBox(height: 15,),
-      //       Text('Root = ${root.toStringAsFixed(3)}',style: const TextStyle(
-      //           color: Colors.white,
-      //           fontWeight: FontWeight.bold,
-      //           fontSize: 20
-      //       ),),
-      //     ],
-      //   ),
-      // ),
+      ),
       backgroundColor: HexColor("DBE2EF"),
     );
+  }
+
+  void calculateGoldenSection() {
+    var mathExpression = TeXParser(equationController.currentEditingValue()).parse();
+    ContextModel cm = ContextModel();
+
+    for (int i = 0; i < iter; i++) {
+      d = r * (xu - xl);
+      x1 = xl + d;
+      x2 = xu - d;
+
+      cm.bindVariableName('x', Number(x1));
+      mathExpression.evaluate(EvaluationType.REAL, cm);
+      double funX1 = mathExpression.evaluate(EvaluationType.REAL, cm);
+
+      cm.bindVariableName('x', Number(x2));
+      mathExpression.evaluate(EvaluationType.REAL, cm);
+      double funX2 = mathExpression.evaluate(EvaluationType.REAL, cm);
+
+      cm.bindVariableName('x', Number(xl));
+      mathExpression.evaluate(EvaluationType.REAL, cm);
+      double funXl = mathExpression.evaluate(EvaluationType.REAL, cm);
+
+      cm.bindVariableName('x', Number(xu));
+      mathExpression.evaluate(EvaluationType.REAL, cm);
+      double funXu = mathExpression.evaluate(EvaluationType.REAL, cm);
+
+      results.add(GoldenSectionModel(iter: i+1,
+          xl: xl,
+          funXl: funXl,
+          x2: x2,
+          funX2: funX2,
+          x1: x1,
+          funX1: funX1,
+          xu: xu,
+          funXu: funXu,
+          d: d));
+
+      if (min == false) { //maximum
+        if (funX2 > funX1) {
+          xu = x1;
+        }
+        else {
+          xl = x2;
+        }
+      }
+      else {
+        if (funX2 > funX1) {
+          xl = x2;
+        }
+        else {
+          xu = x1;
+        }
+      }
+    }
+
   }
 }
--- lib/modules/ch3_screens/optimization_home_screen.dart
+++ lib/modules/ch3_screens/optimization_home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:numerical_analysis_calculator/modules/ch3_screens/golden_section_search_screen.dart';
 import 'package:numerical_analysis_calculator/shared/components/components.dart';
+import 'package:numerical_analysis_calculator/modules/ch3_screens/gradiant_method_screen.dart';
 
 class OptimizationHomeScreen extends StatelessWidget {
 
@@ -10,33 +11,66 @@ class OptimizationHomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(10.0),
-        child: GestureDetector(
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: HexColor("FF7E67"),
-            ),
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                Text(
-                  'Golden Section Search',
-                  style:  TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+        child: Column(
+          children: [
+            GestureDetector(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: HexColor("FF7E67"),
                 ),
-              ],
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Golden Section Search',
+                      style:  TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              onTap: () {
+                navigateTo(context, GoldenSectionSearchScreen());
+              },
             ),
-          ),
-          onTap: () {
-            navigateTo(context, GoldenSectionSearchScreen());
-          },
+            const SizedBox(height: 25,),
+            GestureDetector(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: HexColor("FF7E67"),
+                ),
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Gradient Method',
+                      style:  TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              onTap: () {
+                navigateTo(context, GradientMethodScreen());
+              },
+            ),
+          ],
         ));
   }
 }
